3a6313e0dcde05a958bb6fac3b96f371144aff0a
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-18 11:53:15 +0100

    creazione prodotto fake in PdocutFacotry e aggiunto HasFactory in Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'pages', 'year'];    
+    use HasFactory;
+    protected $fillable = ['name', 'pages', 'year'];
 }
