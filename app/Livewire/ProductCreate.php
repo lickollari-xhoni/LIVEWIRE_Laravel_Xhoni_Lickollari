00c6ad17d1e318d10c23889ebbd2e36d6d56d22f
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-18 14:52:53 +0100

    agiunta di wire:model="name" "pages" e "year" per creare la conessione tra il form e l'inserimetno

--- app/Livewire/ProductCreate.php
+++ app/Livewire/ProductCreate.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class ProductCreate extends Component
 {
+    public $name;
+    public $pages;
+    public $year;
+
     public function render()
     {
         return view('livewire.product-create');
--- resources/views/livewire/product-create.blade.php
+++ resources/views/livewire/product-create.blade.php
@@ -1,21 +1,20 @@
 <div>
     <form>
+        <div class="mb-3">
+            <label class="form-label">Nome Prodotto</label>
+            <input type="text" class="form-control" wire:model="name">
+        </div>
 
-            <div class="mb-3">
-                <label class="form-label">Nome Prodotto</label>
-                <input type="text" class="form-control" name="name">
-            </div>
+        <div class="mb-3">
+            <label class="form-label">Pagine del Prodotto</label>
+            <input type="text" class="form-control" wire:model="pages">
+        </div>
 
-            <div class="mb-3">
-                <label class="form-label">Pagine del Prodotto</label>
-                <input type="text" class="form-control" name="pages">
-            </div>
+        <div class="mb-3">
+            <label class="form-label">Anno del Prodotto</label>
+            <input type="text" class="form-control" wire:model="year">
+        </div>
 
-            <div class="mb-3">
-                <label class="form-label">Anno del Prodotto</label>
-                <input type="text" class="form-control" name="year">
-            </div>
-
-            <button type="submit" class="btn btn-primary">Salva</button>
-        </form>
+        <button type="submit" class="btn btn-primary">Salva</button>
+    </form>
 </div>
