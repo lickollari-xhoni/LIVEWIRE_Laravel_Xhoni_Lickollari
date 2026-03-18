c6dcfba3903116018cb480159fb4efaf9e213822
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-18 14:31:08 +0100

    creazione route di create e edit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
 
+Route::get('/create', [ProductController::class, 'create'])->name('product.create');
+
+Route::get('/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
