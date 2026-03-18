319441141ae215c42a2e17e97d35b8d4cb3594cd
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-18 15:53:07 +0100

    creazione alert con aggiornamento istantaneo dell errore quando cambio campo del click

--- resources/views/livewire/product-create.blade.php
+++ resources/views/livewire/product-create.blade.php
@@ -11,16 +11,31 @@
         <div class="mb-3">
             <label class="form-label">Nome Prodotto</label>
             <input type="text" class="form-control" wire:model.blur="name">
+            @error('name')
+                <div class="alert alert-danger mt-2" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label class="form-label">Pagine del Prodotto</label>
             <input type="text" class="form-control" wire:model.blur="pages">
+            @error('pages')
+                <div class="alert alert-danger mt-2" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label class="form-label">Anno del Prodotto</label>
             <input type="text" class="form-control" wire:model.blur="year">
+            @error('year')
+                <div class="alert alert-danger mt-2" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Salva</button>
