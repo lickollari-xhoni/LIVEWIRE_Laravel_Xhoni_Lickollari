1b676b026819c2d4c47fc3c1008a86c92f053d42
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-18 12:12:58 +0100

    creazione pagina index.blade con tabelle e aggiunta navbar cdn e js in layouts.blade

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,36 @@
+<x-layouts>
+
+    <section class="container mt-5">
+        <div>
+
+            <table class="table">
+                <thead>
+
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome Prodotto</th>
+                        <th scope="col">Numero Pagine</th>
+                        <th scope="col">Anno di Rilascio</th>
+                        <th scope="col">Azioni</th>
+                    </tr>
+                    
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+
+
+        </div>
+    </section>
+
+
+
+</x-layouts>
