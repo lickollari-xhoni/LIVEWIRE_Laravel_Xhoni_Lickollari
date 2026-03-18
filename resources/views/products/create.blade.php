79e1c4e9327560885858cf473b966a8eefbf91bb
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-18 14:46:37 +0100

    creazione componente livewire di ProductCreate e in seguito fatto estrazione da create.blade  e incapsulamento in product-create

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,25 +2,7 @@
 
     <section class="container mt-5">
 
-        <form>
-
-            <div class="mb-3">
-                <label class="form-label">Nome Prodotto</label>
-                <input type="text" class="form-control" name="name">
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label">Pagine del Prodotto</label>
-                <input type="text" class="form-control" name="pages">
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label">Anno del Prodotto</label>
-                <input type="text" class="form-control" name="year">
-            </div>
-
-            <button type="submit" class="btn btn-primary">Salva</button>
-        </form>
+        @livewire('product-create')
 
     </section>
 
